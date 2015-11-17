5f2e9a15a5a53a95689e34078a5b3fcf0969e748
RioShine <tnnghia1994@gmail.com>
2015-11-17 14:49:07 +0700

    Setup project

---
+++ Sample.Test/CalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator;
+
+namespace Sample.Test
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        private Calculators _cal;
+        [SetUp]
+        public void Setup()
+        {
+            _cal = new Calculators();
+        }
+        [Test]
+        public void OnePlusOneEqual2()
+        {
+            Assert.AreEqual(2,_cal.Add(1,1));
+        }
+        [Test]
+        public void TwoPlusTowEqual4()
+        {
+            Assert.AreEqual(4, _cal.Add(2, 2));
+        }
+        [Test]
+        public void FourPlusTowEqual6()
+        {
+            Assert.AreEqual(6, _cal.Add(4, 2));
+        }
+
+    }
+}
